5c13d0e8862c03a16f3c4dcb5e880c51ca19a40a
Teodor Zafirov <36457245+t3dkich@users.noreply.github.com>
2018-03-08 21:39:33 +0200

    Problem1
    Test for Problem1

---
+++ intro-and-basic-syntax/exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProgramTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName = Console.ReadLine();
+
+            int productVolume = int.Parse(Console.ReadLine());
+            int productEnergy = int.Parse(Console.ReadLine());
+            int productSugar = int.Parse(Console.ReadLine());
+
+            double energy = productVolume / 100.00 * productEnergy;
+            double sugars = productVolume / 100.00 * productSugar;
+
+            Console.WriteLine($"{productVolume}ml {productName}:");
+            Console.WriteLine($"{energy}kcal, {sugars}g sugars");
+
+        }
+    }
+}
