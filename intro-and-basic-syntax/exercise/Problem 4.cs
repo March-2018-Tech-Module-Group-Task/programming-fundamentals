306f2e10b87375c5d9d7ae62ec82d46976088ffb
bojidar_bgd <bojidar_bgd@abv.bg>
2018-03-12 22:03:52 +0200

    adding coment

--- intro-and-basic-syntax/exercise/Problem 4.cs
+++ intro-and-basic-syntax/exercise/Problem 4.cs
@@ -6,12 +6,17 @@ namespace E_04._Beverage_Labels
     {
         static void Main(string[] args)
         {
+            //Name of product//
             string name = Console.ReadLine();
+            //Volume in integer//
             int volume = int.Parse(Console.ReadLine());
+            //Energy content value input//
             int energy_content = int.Parse(Console.ReadLine());
+            //Sugar content value input//
             int sugar_content = int.Parse(Console.ReadLine());
-
+            //Output Volume with ml//
             Console.WriteLine($"{volume}ml {name}:");
+            //Output in the certant format//
             Console.WriteLine($"{1.0*energy_content*volume/100}kcal, " +
                 $"{1.0*sugar_content*volume/100}g sugars");
 
