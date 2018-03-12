428846efc86594e138b9ddfd3f51aa26c4e1fac1
bojidar_bgd <bojidar_bgd@abv.bg>
2018-03-12 21:57:53 +0200

    adding problem 4
    Signed-off-by: bojidar_bgd <bojidar_bgd@abv.bg>

---
+++ intro-and-basic-syntax/exercise/Problem 4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace E_04._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy_content = int.Parse(Console.ReadLine());
+            int sugar_content = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{1.0*energy_content*volume/100}kcal, " +
+                $"{1.0*sugar_content*volume/100}g sugars");
+
+        }
+    }
+}
